89c377efdba7737919279ef347f36dec20d7c999
hoang nguyen <hnguyen4187@gmail.com>
2018-05-15 11:38:56 -0400

    posts are linked to user id closes #4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,25 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id 
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
@@ -11,4 +27,9 @@ class PostsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
 end
